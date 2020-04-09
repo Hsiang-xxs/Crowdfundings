db1905b9faac383839e2975066182cb7f6267656
xxs <hsiang.xxs@gmail.com>
2020-04-09 10:55:14 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CrowdFund/>
+  </div>
+</template>
+
+<script>
+import CrowdFund from './components/CrowdFund.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CrowdFund
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
